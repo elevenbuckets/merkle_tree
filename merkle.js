31eb463713f3e3c0805bcec915535f4430da417b
KaiLung Sun <kaisun@elevenbuckets.com>
2019-01-02 21:06:33 -0700

    use class to rewrite

--- merkle.js
+++ merkle.js
@@ -1,155 +1,151 @@
 'use strict';
+
 const hashFunction = require('ethereumjs-utils').keccak256;
 
-var MerkleTree = function() {
-    // in case 'new' was omitted
-    if (!(this instanceof MerkleTree)) {
-        return new MerkleTree();
-    }
+class MerkleTree {
+    constructor() {
+        this.tree = {
+            leaves: [],
+            levels: [],
+            isReady: false
+        };
 
-    var tree = {};
-    tree.leaves = [];
-    tree.levels = [];
-    tree.isReady = false;
+        this.resetTree = () => {
+            this.tree = {};
+            this.tree.leaves = [];
+            this.tree.levels = [];
+            this.tree.isReady = false;
+        };
 
-    this.resetTree = function() {
-        tree = {};
-        tree.leaves = [];
-        tree.levels = [];
-        tree.isReady = false;
-    };
-
-    this.addLeaf = function (value, doHash) {
-        tree.isReady = false;
-        if (doHash) value = hashFunction(value);
-        tree.leaves.push(_getBuffer(value));
-    };
-
-    this.addLeaves = function(values, doHash){
-        tree.isReady = false;
-        values.forEach(function(value){
+        this.addLeaf = (value, doHash) => {
+            this.tree.isReady = false;
             if (doHash) value = hashFunction(value);
-            tree.leaves.push(_getBuffer(value));
-        });
-    };
+            this.tree.leaves.push(this._getBuffer(value));
+        };
 
-    this.makeTree = function() {
-        tree.isReady = false;
-        tree.leaves.sort();
-        var leafCount = tree.leaves.length;
-        if (leafCount > 0) {
-            tree.levels = [];
-            tree.levels.unshift(tree.leaves);
-            while (tree.levels[0].length > 1) {
-                tree.levels.unshift(_calculateNextLevel());
+        this.addLeaves = (values, doHash) => {
+            this.tree.isReady = false;
+            values.forEach((value) => {
+                if (doHash) value = hashFunction(value);
+                this.tree.leaves.push(this._getBuffer(value));
+            });
+        };
+
+        this.makeTree = () => {
+            this.tree.isReady = false;
+            this.tree.leaves.sort();
+            let leafCount = this.tree.leaves.length;
+            if (leafCount > 0) {
+                this.tree.levels = [];
+                this.tree.levels.unshift(this.tree.leaves);
+                while (this.tree.levels[0].length > 1) {
+                    this.tree.levels.unshift(this._calculateNextLevel());
+                }
             }
-        }
-        tree.isReady = true;
-    };
+            this.tree.isReady = true;
+        };
 
-    // Returns the merkle root value for the tree
-    this.getMerkleRoot = function (toHex = false) {
-        if (!tree.isReady || tree.levels.length === 0) return null;
-        if (toHex) {
-            return _buf2hex(tree.levels[0][0]);
-        } else {
-            return tree.levels[0][0];
-        }
-    };
-
-    // Returns the proof for a leaf at the given index as an array of merkle siblings in hex format
-    this.getProof = function (index) {
-        if (!tree.isReady) return null;
-        var currentRowIndex = tree.levels.length - 1;
-        if (index < 0 || index > tree.levels[currentRowIndex].length - 1) return null;  // the index it out of the bounds of the leaf array
-
-        var proof = [];
-        for (var x = currentRowIndex; x > 0; x--) {
-            var currentLevelNodeCount = tree.levels[x].length;
-            // skip if this is an odd end node
-            if (index === currentLevelNodeCount - 1 && currentLevelNodeCount % 2 === 1) {
-                index = Math.floor(index / 2);
-                continue;
-            }
-
-            // determine the sibling for the current index and get its value
-            var isRightNode = index % 2;
-            var siblingIndex = isRightNode ? (index - 1) : (index + 1);
-
-            var sibling = {};
-            var siblingPosition = isRightNode ? 'left' : 'right';
-            var siblingValue = tree.levels[x][siblingIndex].toString('hex');
-            sibling[siblingPosition] = siblingValue;
-            proof.push(sibling);
-            index = Math.floor(index / 2);  // set index to the parent index
-        }
-
-        return proof;
-    };
-
-    this.getLeafCount = function () {
-        return tree.leaves.length;
-    };
-
-    this.getLeaf = function(index) {
-        if (index < 0 || index > tree.leaves.length - 1) return null;
-        return tree.leaves[index];
-    };
-
-    // Takes a proof array, a target hash value, and a merkle root
-    // Checks the validity of the proof and return true or false
-    this.validateProof = function (proof, targetHash, merkleRoot) {
-        targetHash = _getBuffer(targetHash);
-        merkleRoot = _getBuffer(merkleRoot);
-        if (proof.length === 0) return targetHash.toString('hex') === merkleRoot.toString('hex'); // no siblings, single item tree, so the hash should also be the root
-
-        var proofHash = targetHash;
-        for (var x = 0; x < proof.length; x++) {
-            if (proof[x].left) { // then the sibling is a left node
-                proofHash = hashFunction(Buffer.concat([_getBuffer(proof[x].left), proofHash]));
-            } else if (proof[x].right) {
-                proofHash = hashFunction(Buffer.concat([proofHash, _getBuffer(proof[x].right)]));
+        this.getMerkleRoot = (toHex=false) => {
+            if (!this.tree.isReady || this.tree.levels.length === 0) return null;
+            if (toHex) {
+                return this._buf2hex(this.tree.levels[0][0]);
             } else {
-                return false;
+                return this.tree.levels[0][0];
             }
-        }
+        };
 
-        return proofHash.toString('hex') === merkleRoot.toString('hex');
-    };
+        // Returns the proof for a leaf at the given index as an array of merkle siblings in hex format
+        this.getProof = (index) => {
+            if (!this.tree.isReady) return null;
+            let currentRowIndex = this.tree.levels.length - 1;
+            if (index < 0 || index > this.tree.levels[currentRowIndex].length - 1) return null;  // the index it out of the bounds of the leaf array
 
-    function _getBuffer (value) {
-        if (value instanceof Buffer) {
-            return value;
-        } else if (_isHex(value)) {
-            return Buffer.from(value, 'hex');
-        } else { // the value is neither buffer nor hex string, will not process this, throw error
-            throw new Error("Bad hex value - '" + value + "'");
-        }
-    }
+            let proof = [];
+            for (let x = currentRowIndex; x > 0; x--) {
+                let currentLevelNodeCount = this.tree.levels[x].length;
+                // skip if this is an odd end node
+                if (index === currentLevelNodeCount - 1 && currentLevelNodeCount % 2 === 1) {
+                    index = Math.floor(index / 2);
+                    continue;
+                }
 
-    function _isHex (value) {
-        var hexRegex = /^[0-9A-Fa-f]{2,}$/;
-        return hexRegex.test(value);
-    }
+                // determine the sibling for the current index and get its value
+                let isRightNode = index % 2;
+                let siblingIndex = isRightNode ? (index - 1) : (index + 1);
 
-    function _calculateNextLevel () {
-        var nodes = [];
-        var topLevel = tree.levels[0];
-        var topLevelCount = topLevel.length;
-        for (var x = 0; x < topLevelCount; x += 2) {
-            if (x + 1 <= topLevelCount - 1) { // concatenate and hash the pair, add to the next level array
-                nodes.push(hashFunction(Buffer.concat([topLevel[x], topLevel[x + 1]])));
-            } else { // this is an odd ending node, promote up to the next level by itself
-                nodes.push(topLevel[x]);
+                let sibling = {};
+                let siblingPosition = isRightNode ? 'left' : 'right';
+                let siblingValue = this.tree.levels[x][siblingIndex].toString('hex');
+                sibling[siblingPosition] = siblingValue;
+                proof.push(sibling);
+                index = Math.floor(index / 2);  // set index to the parent index
             }
-        }
-        return nodes;
-    }
+            return proof;
+        };
 
-    function _buf2hex(buf){
-        return buf.toString('hex');
-    }
+        this.getLeafCount = () => {
+            return this.tree.leaves.length;
+        };
 
-};
+        this.getLeaf = (index) => {
+            if (index < 0 || index > this.tree.leaves.length - 1) return null;
+            return this.tree.leaves[index];
+        };
+
+        // Takes a proof array, a target hash value, and a merkle root
+        // Checks the validity of the proof and return true or false
+        this.validateProof = (proof, targetHash, merkleRoot) => {
+            targetHash = this._getBuffer(targetHash);
+            merkleRoot = this._getBuffer(merkleRoot);
+            if (proof.length === 0) return targetHash.toString('hex') === merkleRoot.toString('hex');
+
+            let proofHash = targetHash;
+            for (let x = 0; x < proof.length; x++) {
+                if (proof[x].left) { // then the sibling is a left node
+                    proofHash = hashFunction(Buffer.concat([this._getBuffer(proof[x].left), proofHash]));
+                } else if (proof[x].right) {
+                    proofHash = hashFunction(Buffer.concat([proofHash, this._getBuffer(proof[x].right)]));
+                } else {
+                    return false;
+                }
+            }
+
+            return proofHash.toString('hex') === merkleRoot.toString('hex');
+        };
+
+        this._getBuffer = (value) => {
+            if (value instanceof Buffer) {
+                return value;
+            } else if (this._isHex(value)) {
+                return Buffer.from(value, 'hex');
+            } else { // the value is neither buffer nor hex string, will not process this, throw error
+                throw new Error("Bad hex value - '" + value + "'");
+            }
+        };
+
+        this._isHex = (value) =>  {
+            let hexRegex = /^[0-9A-Fa-f]{2,}$/;
+            return hexRegex.test(value);
+        };
+
+        this._calculateNextLevel = () => {
+            let nodes = [];
+            let topLevel = this.tree.levels[0];
+            let topLevelCount = topLevel.length;
+            for (let x = 0; x < topLevelCount; x += 2) {
+                if (x + 1 <= topLevelCount - 1) { // concatenate and hash the pair, add to the next level array
+                    nodes.push(hashFunction(Buffer.concat([topLevel[x], topLevel[x + 1]])));
+                } else { // this is an odd ending node, promote up to the next level by itself
+                    nodes.push(topLevel[x]);
+                }
+            }
+            return nodes;
+        };
+
+        this._buf2hex = (buf) => {
+            return buf.toString('hex');
+        };
+    }
+}
 
 module.exports = MerkleTree;
--- test_mk.js
+++ test_mk.js
@@ -2,37 +2,43 @@ const MerkleTree = require('./merkle.js');
 const keccak256 = require('ethereumjs-utils').keccak256;
 
 const merkleTree = new MerkleTree();
-let leaves = ['a', 'b', 'c', 'd', 'e'].map((x)=>keccak256(x).toString('hex'));
-merkleTree.addLeaves(leaves);
+merkleTree.addLeaf('a', true);  // the 'true' tell addLeaf to convert to hash
+merkleTree.addLeaves(['b', 'c', 'd', 'e'].map((x)=>keccak256(x).toString('hex')));
 merkleTree.makeTree();
 
-console.log("Hashes of 'a', 'b', 'c', 'd', 'e':");
+console.log("Added hashes of 'a', 'b', 'c', 'd', 'e' to the tree:");
 ['a', 'b', 'c', 'd', 'e'].forEach( (x) => console.log(x + ':' + keccak256(x).toString('hex')));
 console.log('\nIndex of leaves are sorted by their hash in the tree:')  // ...not the order to add them
 for (var i=0; i<5; i++){
     console.log(merkleTree.getLeaf(i).toString('hex'));
 }
+console.log('\nNumber of leaves: ' + merkleTree.getLeafCount());
+
 let mroot = merkleTree.getMerkleRoot();
 
 // 'good' examples
-console.log('\nGood examples (all true):');
+console.log('\nGood validation examples (all true):');
 let proof, target, isValid;
 for (var i = 0; i < 5; i ++){
     proof = merkleTree.getProof(i);
     // console.dir(proof);
     target = merkleTree.getLeaf(i);
     isValid = merkleTree.validateProof(proof, target, mroot);
-    console.log(`${i+1}: ${isValid} (${target.toString('hex')})`);
+    console.log(`${i+1}: ${isValid} (target hash: ${target.toString('hex')})`);
 }
 
 // 'bad' examples
-console.log('\nBad examples (all false):');
+console.log('\nBad validation examples (all false):');
 proof = merkleTree.getProof(0);
 target = keccak256('evil tx 0').toString('hex');
 isValid = merkleTree.validateProof(proof, target, mroot);
-console.log(`1: ${isValid} (${target.toString('hex')})`);
+console.log(`1: ${isValid} (target hash: ${target.toString('hex')})`);
 
 proof = merkleTree.getProof(4);
-target = merkleTree.getLeaf(3);
-isValid = merkleTree.validateProof(proof, target, mroot);  // use wrong proof or tx
-console.log(`2: ${isValid} (${target.toString('hex')})`);
+target = merkleTree.getLeaf(3);  // use a wrong target hash
+isValid = merkleTree.validateProof(proof, target, mroot);
+console.log(`2: ${isValid} (target hash: ${target.toString('hex')})`);
+
+console.log('\ntest resetTree() (should return null):');
+merkleTree.resetTree();
+console.log(merkleTree.getMerkleRoot());  // should return null
