abc3b6f82ec63f7b00671b11799f61377245d313
KaiLung Sun <kaisun@elevenbuckets.com>
2019-01-04 14:14:10 -0700

    add fnuction to upload MerkleProof (yet to be tested)

--- truffle/contracts/MerkleTreeValidate.sol
+++ truffle/contracts/MerkleTreeValidate.sol
@@ -1,10 +1,37 @@
 pragma solidity ^0.4.24;
 
 contract MerkleTreeValidate {
+    address public owner;
+    uint constant maxLengthOfProof = 1024;
+    struct MerkleRoot {
+        bytes32 root;
+        bytes32 bufmessage;
+    }
+
+    mapping (uint256 => MerkleRoot) public merkleRootInfo;
+
+    modifier ownerOnly() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    function uploadMerkleRoot(uint256 blockNo, bytes32 root, bytes32 bufmessage) external ownerOnly returns (bool) {
+        require(blockNo < block.number);
+        MerkleRoot memory newRoot;
+        newRoot.root = root;
+        newRoot.bufmessage = bufmessage;
+        merkleRootInfo[blockNo] = newRoot;
+        return;
+    }
 
     function validate(bytes32[] memory proof, bool[] memory isLeft, bytes32 targetLeaf, bytes32 merkleRoot) public pure returns (bool) {
+        require(proof.length < maxLengthOfProof);
         require(proof.length == isLeft.length);
-        
+
         bytes32 targetHash = targetLeaf;
         for (uint256 i = 0; i < proof.length; i++) {
             bytes32 proofEle = proof[i]; 
@@ -16,7 +43,6 @@ contract MerkleTreeValidate {
                 return false;
             }
         }
-
         return targetHash == merkleRoot;
     }
 }
